cf8083ab18a2c244d92f4195e853a70aa77f74bc
ferny <ferdoohd2@gmail.com>
2025-08-17 14:52:56 -0700

    Savepoint!

--- hyprmacroboard/config.h
+++ hyprmacroboard/config.h
@@ -1,23 +1,15 @@
 #pragma once
 
+#define ENCODER_MAP_KEY_DELAY 10
 // matrix size
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 3
 
-#define DIODE_DIRECTION COL2ROW
-
-// encoder details
 #define NUM_ENCODERS 1
-#define NUM_ENCODERS_PER_SIDE 1
-#define ENCODERS_A_PINS { F6 }
-#define ENCODERS_B_PINS { F7 }
-#define ENCODER_RESOLUTION 4
-
+#define NUM_DIRECTIONS 2
 // oled display
-#define OLED_DISPLAY_128x32
-#define OLED_SDA_PIN D1
-#define OLED_SCL_PIN D0
-
-#define MATRIX_ROW_PINS {B1, B3, B2, B6, B5}
-#define MATRIX_COL_PINS {F4, F5, B4}
+#define OLED_BRIGHTNESS 128
+#define OLED_DISPLAY_128X32
+#define I2C1_SDA_PIN D1
+#define I2C1_SCL_PIN D0
 
--- hyprmacroboard/hyprmacroboard.h
+++ hyprmacroboard/hyprmacroboard.h
@@ -2,4 +2,12 @@
 
 #include "quantum.h"
 
+// matrix size
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 3
 
+
+// oled display
+#define OLED_DISPLAY_128X32
+#define I2C1_SDA_PIN D1
+#define I2C1_SCL_PIN D0
